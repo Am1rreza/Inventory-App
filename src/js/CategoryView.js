d3d40aaa524018290abbffe7831308d56bdcae29
Amirreza <amirreza.m13825@gmail.com>
2023-01-05 13:50:40 +0330

    createCategoryList method added

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -20,16 +20,41 @@ class CategoryView {
     const title = this.title.value;
     const description = this.description.value;
 
-    if (!title || !desc) return;
+    if (!title || !description) {
+      alert("Please fill out the both inputs");
+      return;
+    }
     // save to storage
     Storage.saveCategory({ title, description });
     // fill categories
-    this.categories = Storage.getAllCategories();
+    this.setApp();
     // update DOM : update the select option
+    this.createCategoriesList();
+    // clear Inputs
+    this.title.value = "";
+    this.description.value = "";
   }
 
   setApp() {
     this.categories = Storage.getAllCategories();
+  }
+
+  createCategoriesList() {
+    // set default category
+    let result = `<option class="bg-slate-500 text-slate-300" value="">
+    Select a Category
+  </option>`;
+
+    // add the others categories
+    this.categories.forEach((category) => {
+      result += `<option class="bg-slate-500 text-slate-300" value=${category.id}>
+    ${category.title}
+  </option>`;
+    });
+
+    // attach the categoris to DOM
+    const categoriesDOM = document.getElementById("product-category");
+    categoriesDOM.innerHTML = result;
   }
 }
 
--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,6 @@ import CategoryView from "./CategoryView.js";
 // DOM in first load
 document.addEventListener("DOMContentLoaded", () => {
   CategoryView.setApp();
+  //create categories option
+  CategoryView.createCategoriesList();
 });
