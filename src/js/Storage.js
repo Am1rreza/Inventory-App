041c74ccd3ad4ec9798be5731fe01d9428768ce6
Amirreza <amirreza.m13825@gmail.com>
2023-01-05 11:58:32 +0330

    getAllCategories method added to Storage class

--- src/js/Storage.js
+++ src/js/Storage.js
@@ -1,1 +1,28 @@
-export default class Storage {}
+const categories = [
+  {
+    id: 1,
+    title: "frontend",
+    description: "frontend of app",
+    createdAt: "2023-01-05T08:23:36.397Z",
+  },
+  {
+    id: 2,
+    title: "backend",
+    description: "backend of app",
+    createdAt: "2023-01-05T08:23:52.736Z",
+  },
+];
+
+export default class Storage {
+  static getAllCategories() {
+    // get categories from localStorage
+    const savedCategories =
+      JSON.parse(localStorage.getItem("categories")) || [];
+    // desending sort on categories
+    const sortedCategories = savedCategories.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+
+    return sortedCategories;
+  }
+}
